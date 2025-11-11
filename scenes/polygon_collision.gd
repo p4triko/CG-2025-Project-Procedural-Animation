07a53f2148357d80d552a7a126c1def07c4ad6d1
Juhan Oskar Hennoste <juhanoskar@gmail.com>
2025-11-11 20:21:20 +0200

    Add IK physics test using SimNode and AnimatableBody2D

--- scenes/polygon_collision.gd
+++
@@ -1,9 +0,0 @@
-extends PhysicsBody2D
-
-func _ready():
-	# Automatically create collision shapes for all Polygon2D child nodes
-	for child in get_children():
-		if child is Polygon2D:
-			var collision_polygon := CollisionPolygon2D.new()
-			collision_polygon.polygon = child.polygon
-			add_child(collision_polygon)
---
+++ scenes/track_mouse.gd
@@ -0,0 +1,4 @@
+extends RemoteTransform2D
+
+func _physics_process(_delta: float):
+	global_position = get_global_mouse_position()
