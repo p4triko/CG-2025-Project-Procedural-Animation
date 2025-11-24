a692e4d0c0ca7a46dcc70d2813dd9f6575fd0c28
Artjom Geimanen <artjom.geimanen@gmail.com>
2025-11-24 16:32:45 +0200

    Working Recursive Raycast

--- objects/RecursiveRayCast2D/RecursiveRayCast2D.gd
+++ objects/RecursiveRayCast2D/RecursiveRayCast2D.gd
@@ -2,22 +2,25 @@
 class_name RecursiveRayCast2D
 extends Node2D
 
-@export var target_positon: Vector2:
+## In local space
+@export var target_positon: Vector2 = Vector2(0, 50):
 	set(v):
 		target_positon = v
 		queue_redraw()
 @export var max_iterations: int = 20
+@export var exclude: Array[RID]
+@export var epsilon: float = 1.0
 
 @export_tool_button("Run") var a = func (): print(get_collisions())
 
-var saved_hits: Array[Array]
+var saved_hits: Array[Array] # For visualization
 
 func get_collisions() -> Array[Array]:
 	var space_state := get_world_2d().direct_space_state
 	
 	var query := PhysicsRayQueryParameters2D.new()
-	query.hit_from_inside = true
-	query.to = target_positon
+	query.to = global_position + target_positon
+	query.exclude = exclude
 	var curr_pos: Vector2 = global_position
 	
 	var hits: Array[Array]
@@ -29,12 +32,12 @@ func get_collisions() -> Array[Array]:
 		i+=1; if i > max_iterations: break # Exit if too many iterations
 		
 		hits.append([result.position, result.normal])
-		curr_pos = result.position
+		curr_pos = result.position + global_position.direction_to(global_position + target_positon).normalized() * epsilon
 	saved_hits = hits
 	queue_redraw()
 	return hits
 
 func _draw() -> void:
-	draw_line(Vector2.ZERO, target_positon - global_position, Color(1, 0.2, 0.2, 0.7), 1.5)
+	draw_line(Vector2.ZERO, target_positon, Color(1, 0.2, 0.2, 0.7), 1.5)
 	for i in saved_hits:
 		draw_circle(i[0] - global_position, 2, Color(1, 0.2, 0.2, 0.7), false, 1)
