74432b74055db869823b5a382a9f9abcdc1036e4
Juhan Oskar Hennoste <juhanoskar@gmail.com>
2026-01-14 20:19:07 +0200

    Add logic to swap between palettes

--- scenes/main.gd
+++ scenes/main.gd
@@ -1,12 +1,14 @@
 extends Node2D
 
+@onready var palette_material: ShaderMaterial = load("res://shaders/palette_swap.tres")
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+@export var palettes: Array[PackedColorArray] = []
+var current_palette: int = 0
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	if Input.is_action_just_pressed("restart"):
 		get_tree().reload_current_scene()
+	
+	if Input.is_action_just_pressed("swap_palette"):
+		current_palette = (current_palette + 1) % len(palettes)
+		palette_material.set_shader_parameter("palette", palettes[current_palette])
