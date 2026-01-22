ca5945b49ed858d38c9dcd9f4452f96028de2e37
Patrick <168976850+p4triko@users.noreply.github.com>
2026-01-22 16:58:55 +0200

    Tentacle snatching smoothly, test out spider swap

--- scripts/spineevolve.gd
+++ scripts/spineevolve.gd
@@ -3,12 +3,14 @@ extends Spine
 var current_draw_state: int = 0 # Which part of the draw state we are at, basically to show the evolution of the spine
 
 enum DrawStates {
+	POINTS, # Draw chain links basically.
 	SPINE_ONLY, # Simply draw the line that basically acts as the spine.
 	BODY_OUTLINES, # Circle outlines that make up the body.
 	FINAL_SPINE # The final version of the spine with proper coloring
 }
 
 var spine_renders: Array[DrawStates] = [
+	DrawStates.POINTS,
 	DrawStates.SPINE_ONLY,
 	DrawStates.BODY_OUTLINES,
 	DrawStates.FINAL_SPINE
@@ -29,9 +31,12 @@ func _draw():
 	var draw_state = spine_renders[current_draw_state]
 
 	match draw_state:
+		DrawStates.POINTS:
+			for i in  range(1, segment_count):
+				draw_circle(pts[i], 2.0, Color.RED, false)
 		DrawStates.SPINE_ONLY:
 			for i in range(1, segment_count):
-				draw_line(pts[i - 1], pts[i], Color.RED)
+				draw_line(pts[i - 1], pts[i], Color.RED, 2.0)
 
 		DrawStates.BODY_OUTLINES:
 			for i in range(1, segment_count):
--- scripts/tentacle.gd
+++ scripts/tentacle.gd
@@ -8,11 +8,15 @@ extends Node2D
 
 @onready var line_2d: Line2D = $Line2D 
 @onready var tip_point: Marker2D = $Line2D/Tip
+@export var apply_waviness: bool = true
 
 @export_group("Behavior Settings")
 @export var follows_mouse: bool = false
+@export var detection: float = 400
+@export var enemy: CharacterBody2D # In that the case the player is the enemy.
+@export var hostile: bool = false
 
-# Assign which direction the tentacle faces by default, for example maybe want it to wonder in the ceiling.
+# Assign which direction the tentacle faces by default, for example maybe want it to wonder in the ceiling.d
 @export_range(-360, 360) var rest_angle: float = 90.0
 
 @export_group("Tentacle Settings")
@@ -25,12 +29,19 @@ extends Node2D
 @export_range(0.0, 5, 0.1) var wave_frequency: float = 2.0
 @export_range(0.0, 10.0, 0.1) var wave_speed: float = 3.0
 
+@export_group("Attack Smoothing")
+@export_range(0.0, 30.0, 0.1) var snap_speed: float = 10.0
+@export_range(0.0, 30.0, 0.1) var return_speed: float = 6.0
+
+var aim_target: Vector2
+var was_aiming = false
+
 var segments: Array[Vector2] = []
 var segment_lengths: Array[float] = []
 var wave_time: float = 0.0
-@export var apply_waviness: bool = true
 
 func _ready():
+	aim_target = calculate_pos()
 	var segment_len = max_length / num_segments 
 	
 	for i in range(num_segments + 1):
@@ -43,35 +54,63 @@ func _ready():
 		line_2d.add_point(Vector2.ZERO)
 
 func _process(delta: float) -> void:
-	var target_pos: Vector2
-	
-	if follows_mouse:
-		var mouse_pos: Vector2 = get_global_mouse_position()
-		var direction: Vector2 = mouse_pos - global_position
-		if direction.length() > max_length:
-			target_pos = global_position + direction.normalized() * max_length
-		else:
-			target_pos = mouse_pos
-	else:
-		var rad = deg_to_rad(rest_angle)
-		var dir = Vector2.RIGHT.rotated(rad)
-		target_pos = global_position + (dir * max_length * 0.9)
+	var target: Vector2 = calculate_pos() # Fallback to idle state.
+	var aiming = false # Is the tentacle aiming towards the mouse or the player. Mouse is more for testing purposes.
 
-	solve_ik(target_pos)
-	if apply_waviness:
-		apply_waves(delta)
-	
-	# Pinning
 	if follows_mouse:
-		apply_waviness = false
-		segments[-1] = target_pos
-		
+		aiming = true
+		# Tentacle will aim at the mouse, but not stretching outside the maximum length.
+		target = set_and_clamp_to_length(get_global_mouse_position())
+
+	elif hostile and enemy != null:
+		# Distance between enemy / player and the tentacle.
+		var dist_enemy = global_position.distance_to(enemy.global_position)
+		if dist_enemy < detection:
+			aiming = true
+			target = set_and_clamp_to_length(enemy.global_position)
+		else:
+			# Player is not in range, so we return back to idle state.
+			target = calculate_pos()
+	else:
+		# Nothing is really happening, neither attacking / following, lets just idle around.
+		target = calculate_pos()
+
+	# How quickly should the tentacle respond.
+	var chase_factor: float = 0.0
+
+	if aiming:
+		chase_factor = snap_speed
+	else: 
+		chase_factor = return_speed
+
+	# Frame independent smoothing, exponential decay.
+	var alpha = 1.0 - exp(-chase_factor * delta)
+	aim_target = aim_target.lerp(target, alpha)
+
+	solve_ik(aim_target)
+
+	if apply_waviness and not aiming:
+		apply_waves(delta)
+
 	update_line_visuals()
 
 	if tip_point:
 		tip_point.global_position = segments[-1]
-	
+
 	queue_redraw()
+
+# Set and also clamp the length so the tentacle doesnt stretch outside it's contraints.
+func set_and_clamp_to_length(target: Vector2) -> Vector2:
+	var dir = target - global_position
+	if dir.length() > max_length:
+		return global_position + dir.normalized() * max_length
+	return target # Already in reach, so we don't need to do anything.
+
+# Return the idle position for the tip of the tentacle.
+func calculate_pos() -> Vector2:
+	var rad = deg_to_rad(rest_angle)
+	var dir = Vector2.RIGHT.rotated(rad)
+	return global_position + (dir * max_length * 0.9)
 
 func solve_ik(target: Vector2) -> void:
 	segments[-1] = target # Snap tip to the target
@@ -89,7 +128,7 @@ func solve_ik(target: Vector2) -> void:
 
 		for i in range(num_segments):
 			var dir = (segments[i + 1] - segments[i]).normalized()
-			segments[i+1] = segments[i] + dir * segment_lengths[i]
+			segments[i + 1] = segments[i] + dir * segment_lengths[i]
 
 func apply_waves(delta: float) -> void:
 	if wave_amplitude <= 0: return
