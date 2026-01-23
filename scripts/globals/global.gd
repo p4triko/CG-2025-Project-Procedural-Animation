2d8cbbe8d7b68c2a9585468c2da48c52dab5affe
Artjom Geimanen <artjom.geimanen@gmail.com>
2026-01-23 10:21:39 +0200

    Fullscreen key

--- scripts/globals/global.gd
+++ scripts/globals/global.gd
@@ -14,3 +14,10 @@ func reset_scene():
 	draw_bones = false
 	draw_nodes = false
 	palette_enabled = true
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("fullscreen"):
+		if DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_WINDOWED:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+		else:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
